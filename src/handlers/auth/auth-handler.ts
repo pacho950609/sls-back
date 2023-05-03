6311ed18f628ab4a3975df711d47f38d341fe1ae
Francisco Ricaurte <ricaurte95fj@gmail.com>
2023-05-03 12:42:08 -0500

    Add auth doc

--- src/handlers/auth/auth-handler.ts
+++ src/handlers/auth/auth-handler.ts
@@ -3,31 +3,69 @@ import { handlerWrapper } from 'utils/wrapper';
 import { signUp, login } from '../../services/auth/auth-service';
 
 /**
- * Create a new users
+ * @api {post} /signup Create user
+ * @apiDescription Create a new user and return a token
+ * @apiName signUpHandler
+ * @apiGroup Auth
+ * @apiHeader {String} Authorization User token
+ * @apiBody {String} email User email 
+ * @apiBody {String} password User password.
+ * @apiParamExample {json} Body-Example:
+ *  {
+        email: "test@gmail.com",
+        password: "test123"
+ *  } 
+ * @apiPermission Client
+ * @apiSuccessExample Success-Response:
+ * {
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+ * }  
  */
-export const signUpHandler = handlerWrapper({ 
-    cors: true,  
-    validateBody: J.object({
-        email: J.string().required(),
-        password: J.string().required(),
-    }),
-}, async (event, manager) => {
+export const signUpHandler = handlerWrapper(
+    {
+        cors: true,
+        validateBody: J.object({
+            email: J.string().required(),
+            password: J.string().required(),
+        }),
+    },
+    async (event, manager) => {
         const { email, password } = event.body;
         const token = await signUp(email, password, manager);
         return { token };
-});
+    },
+);
 
 /**
- * Validate user credentials and return a token
+ * @api {post} /login Login
+ * @apiDescription Validate user credentials and return a token
+ * @apiName loginHandler
+ * @apiGroup Auth
+ * @apiHeader {String} Authorization User token
+ * @apiPermission Client
+ * @apiBody {String} email User email 
+ * @apiBody {String} password User password.
+ * @apiParamExample {json} Body-Example:
+ *  {
+        email: "test@gmail.com",
+        password: "test123"
+ *  } 
+ * @apiSuccessExample Success-Response:
+ * {
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+ * }  
  */
-export const loginHandler = handlerWrapper({ 
-    cors: true,
-    validateBody: J.object({
-        email: J.string().required(),
-        password: J.string().required(),
-    }),
-}, async (event, manager) => {
+export const loginHandler = handlerWrapper(
+    {
+        cors: true,
+        validateBody: J.object({
+            email: J.string().required(),
+            password: J.string().required(),
+        }),
+    },
+    async (event, manager) => {
         const { email, password } = event.body;
         const token = await login(email, password, manager);
         return { token };
-});
+    },
+);
